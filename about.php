b53e2a15062eb5611cd64e8f9748d5c99d514532
Richard Begin <richard.begin@triosstudent.com>
2025-06-26 20:49:04 -0400

    Uploaded about.php

---
+++ about.php
@@ -0,0 +1,5 @@
+Bloomix is Token Redemption System.
+ 
+It can by Administation to issue color token system. Each token will have respected point value (ex: pink token = 5 points, purple = 10, etc..)
+
+User can redeem their token for product. 
